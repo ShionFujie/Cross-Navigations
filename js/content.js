875553986b38172e00e84560ff3f34f93ece2513
shion.t.fujie <tonaluna32966@gmail.com>
2020-04-10 15:31:18 +0900

    bind Thesaurus.com

--- js/content.js
+++ js/content.js
@@ -9,13 +9,14 @@ function main() {
 
 function findCapitalInput({ hostname, pathname }) {
   // YouTube
-  if (hostname == "www.youtube.com" && ["/", "/watch"].includes(pathname)) {
+  if (hostname == "www.youtube.com" && ["/", "/watch"].includes(pathname))
     return $("input#search");
-  }
 
   // Google Translate
   if (hostname == "translate.google.com" && pathname == "/")
     return $("#source");
+
+  if (hostname == "www.thesaurus.com") return $("input[type='search']");
 
   return null;
 }
