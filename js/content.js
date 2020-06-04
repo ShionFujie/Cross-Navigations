54f0a68590e3240423562b00e17f71b9ed2f13c0
shion.t.fujie <tonaluna32966@gmail.com>
2020-06-04 20:25:45 +0900

    bind url-dependent inputs

--- js/content.js
+++ js/content.js
@@ -17,7 +17,22 @@ function findCapitalInput({ hostname, pathname }) {
   if (hostname == "www.thesaurus.com") return $("input[type='search']");
   if (hostname == "www.google.com" && pathname == "/search")
     return $("input[name='q']");
-  if (hostname == "www.wordhippo.com") return $("input#wordformword");
+  if (hostname == "www.wordhippo.com") {
+    const pathsIdsTable = {
+      "another-word-for": "anotherword",
+      "the-opposite-of": "oppositeof",
+      "the-meaning-of-the-word": "definitionword",
+      "words-that-rhyme-with": "rhymeword",
+      "sentences-with-the-word": "sentenceword",
+      "how-do-you-pronounce-the-word": "pronounceword"
+    };
+    for (let [path, id] of Object.entries(pathsIdsTable)) {
+      if (pathname.startsWith(`/what-is/${path}`))
+        return $(`input#${id}`)
+    }
+
+    return $("input#wordformword");
+  }
 
   return null;
 }
