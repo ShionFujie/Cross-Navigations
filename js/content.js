8ede6c535ad9bd04204edf52f58e8411766136d1
shion.t.fujie <tonaluna32966@gmail.com>
2020-06-04 20:58:17 +0900

    flattern entries

--- js/content.js
+++ js/content.js
@@ -18,15 +18,15 @@ function findCapitalInput({ hostname, pathname }) {
   if (hostname == "www.google.com" && pathname == "/search")
     return $("input[name='q']");
   if (hostname == "www.wordhippo.com") {
-    const pathsIdsTable = {
-      "another-word-for": ["anotherword", "synonymsMenuDiv"],
-      "the-opposite-of": ["oppositeof", "antonymsMenuDiv"],
-      "the-meaning-of-the-word": ["definitionword", "meaningMenuDiv"],
-      "words-that-rhyme-with": ["rhymeword", "rhymeMenuDiv"],
-      "sentences-with-the-word": ["sentenceword", "sentenceMenuDiv"],
-      "how-do-you-pronounce-the-word": ["pronounceword", "pronounceMenuDiv"]
-    };
-    for (let [path, [inputId, divId]] of Object.entries(pathsIdsTable)) {
+    const pathsIds = [
+      ["another-word-for", "anotherword", "synonymsMenuDiv"],
+      ["the-opposite-of", "oppositeof", "antonymsMenuDiv"],
+      ["the-meaning-of-the-word", "definitionword", "meaningMenuDiv"],
+      ["words-that-rhyme-with", "rhymeword", "rhymeMenuDiv"],
+      ["sentences-with-the-word", "sentenceword", "sentenceMenuDiv"],
+      ["how-do-you-pronounce-the-word", "pronounceword", "pronounceMenuDiv"]
+    ];
+    for (let [path, inputId, divId] of pathsIds) {
       if (
         pathname.startsWith(`/what-is/${path}`) &&
         $(`#${divId}`).css("display") === "block"
