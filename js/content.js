81cc65f10d1280735a9a365b88abf6aa1c0f2649
shion.t.fujie <tonaluna32966@gmail.com>
2020-06-04 20:54:28 +0900

    bind url-free inputs

--- js/content.js
+++ js/content.js
@@ -19,19 +19,32 @@ function findCapitalInput({ hostname, pathname }) {
     return $("input[name='q']");
   if (hostname == "www.wordhippo.com") {
     const pathsIdsTable = {
-      "another-word-for": "anotherword",
-      "the-opposite-of": "oppositeof",
-      "the-meaning-of-the-word": "definitionword",
-      "words-that-rhyme-with": "rhymeword",
-      "sentences-with-the-word": "sentenceword",
-      "how-do-you-pronounce-the-word": "pronounceword"
+      "another-word-for": ["anotherword", "synonymsMenuDiv"],
+      "the-opposite-of": ["oppositeof", "antonymsMenuDiv"],
+      "the-meaning-of-the-word": ["definitionword", "meaningMenuDiv"],
+      "words-that-rhyme-with": ["rhymeword", "rhymeMenuDiv"],
+      "sentences-with-the-word": ["sentenceword", "sentenceMenuDiv"],
+      "how-do-you-pronounce-the-word": ["pronounceword", "pronounceMenuDiv"]
     };
-    for (let [path, id] of Object.entries(pathsIdsTable)) {
-      if (pathname.startsWith(`/what-is/${path}`))
-        return $(`input#${id}`)
+    for (let [path, [inputId, divId]] of Object.entries(pathsIdsTable)) {
+      if (
+        pathname.startsWith(`/what-is/${path}`) &&
+        $(`#${divId}`).css("display") === "block"
+      )
+        return $(`input#${inputId}`);
     }
 
-    return $("input#wordformword");
+    const divIdsQueries = [
+      ["translateMenuDiv", "textarea"],
+      ["matchMenuDiv", "input[type='text']"],
+      ["typeMenuDiv", "input[type='text']"]
+    ];
+    for (let [divId, query] of divIdsQueries) {
+      const div = $(`#${divId}[style*='display: block']`);
+      if (div.length > 0) return div.find(query);
+    }
+
+    return null;
   }
 
   return null;
