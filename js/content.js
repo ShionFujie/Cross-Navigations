8292fbe02f1b95c9e246cee1677b7f5233fd9b12
shion.t.fujie <tonaluna32966@gmail.com>
2020-06-07 17:10:44 +0900

    dedicated function

--- js/content.js
+++ js/content.js
@@ -6,11 +6,7 @@ document.onkeydown = event => {
     return;
   }
   if (code.startsWith("Digit") && shiftKey && altKey && code[5] !== "0") {
-    const menuoption = $(
-      `div[class^="menuoption"]:eq(${parseInt(code[5]) - 1})`
-    );
-    if (menuoption.attr("onclick") === undefined) menuoption.find('a').get(0).click();
-    else menuoption.click();
+    navigateToWordHippoTab(parseInt(code[5]) - 1);
     return;
   }
 };
@@ -20,6 +16,14 @@ function focusCapitalInput() {
   if (capitalInput != null) {
     capitalInput.focus();
   }
+}
+
+function navigateToWordHippoTab(index) {
+  const menuoption = $(`div[class^="menuoption"]:eq(${index})`);
+  const clickable = menuoption.attr("onclick")
+    ? menuoption
+    : menuoption.find("a").get(0);
+  clickable.click();
 }
 
 function findCapitalInput({ hostname, pathname }) {
